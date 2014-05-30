722e8d33f9e8cf055fa58098f4783ef81a841b71
bulu <bulukani.mlalazi@gmail.com>
2014-05-31 00:48:11 +1000

    plot 1 finished and working

--- plot1.R
+++ plot1.R
@@ -1,57 +1,41 @@
 #instructions for making plot 1
 
-# Please add the working Dir in which the files are located.
-# This is currently set to a subdirectory called data.
-workingDir <- "./Data"
+# Change the value of workingDir to that of the directory that contains 
+# the "household_power_consumption.txt" source data.
 
-# Sets working directory to the location specified in teh workingDir variable
+workingDir <- "<insert name of directory containing dataset here>"
+
+# Sets working directory to the location specified in the workingDir variable
 
 setwd(workingDir)
 
-# Read the data into R
-power_data <- read.table("household_power_consumption.txt", header=T, sep=";")
-dataClasses <- ("date", "time", "numeric", "numeric")
+# Read in only the rows corresponding to the relevant dates 2007-02-01 and 2007-02-02. 
 
-# the relevant dates: * We will only be using data from the dates 2007-02-01 and 2007-02-02. 
-
-  # this represents rows: 66637 - 98316 in the original dataset
-  # in the final script, these are the only rows that I will read in
-power_data2 <- read.table("household_power_consumption.txt", header=T, sep=";", skip = 66636, nrow = 8640)
-# this works but you will have to read the names in separately as well
-
+  # Names of the columns in the final dataset
 name_list <- c("Date", "Time", "Global_active_power", 
                "Global_reactive_power", "Voltage", "Global_intensity",
                "Sub_metering_1" , "Sub_metering_2", "Sub_metering_3")
 
-#This leaves us with: the final import statment
-power_dataset <- read.table("household_power_consumption.txt", header=T, sep=";", 
+  # data read-in command
+power_data_subset <- read.table("household_power_consumption.txt", header=T, sep=";", 
                             skip = 66636, nrow = 8640, col.names=name_list)
 
-#search for the date "2007-02-02*
-date2 <- grep("2/2/2007", power_data$Date) #4320 for this search
-date3 <- grep("[1-2]/2/2007", power_data$Date) #8640 for this search (double the previous)
+#search for the data requred for the cart, corresponding to the date "2007-02-01"
+date <- grep("[1-2]/2/2007", power_data_subset$Date) #4320 for this search
 
-#now subset the data by rows with the required dates
-power_subset <- power_data[date3,] 
-
-##           Charting
-# This is the basic chart when you have got the data into the right classes
-
-# note -> use grep to find the rows where the date is "1/2/2007" and make that equal
-# to date
-date <- 1:4320 
-
-hist(power_data22$Global_active_power[date], col="red")
-
-# now to modify the histogram parameters
-hist(power_data22$Global_active_power[1:4320], col="red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
-
-# the one above looks like a winner but there might be more e.g.
-plot = F,
-#This then allows you to divert the output to a PNG file
+# This is the filename of the chart to be created
 png(filename = "plot1.png")
 
-# useful reference: http://www.statmethods.net/advgraphs/axes.html
+# This is the formatted histogram to produce
+hist(power_data_subset$Global_active_power[date], col="red", 
+     main = "Global Active Power", 
+     xlab = "Global Active Power (kilowatts)")
+
+# This shuts down output to the PNG file when done
+dev.off()
+
+
+
 
 
 
